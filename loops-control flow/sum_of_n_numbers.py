ed64f82863bd826ab71cce23083fb0f25f42fa5d
HIMANSHU-SAINI1 <himanshusainibkbiet2022@gmail.com>
2025-04-25 12:21:41 +0530

    python

--- loops-control flow/sum_of_n_numbers.py
+++ loops-control flow/sum_of_n_numbers.py
@@ -8,12 +8,22 @@
 
 
 
-n=int(input("enter numbers"))
-i=0
+# n=int(input("enter numbers"))
+# i=0
+# sum=0
+# print("enter",n,"numbers")
+# while i<n:
+#     i+=1
+#     x=int(input(""))
+#     sum+=x
+# print("sum",sum)
+
+
+
+# sum of n natural numbers using for loop
+
+n=int(input("enter number"))
 sum=0
-print("enter",n,"numbers")
-while i<n:
-    i+=1
-    x=int(input(""))
-    sum+=x
-print("sum",sum)
+for i in range(1,n+1):
+    sum=sum+i
+print("sum is:",sum)
