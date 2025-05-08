f7ed442c16245d31f5100c005d38a28c6ab360e6
HIMANSHU-SAINI1 <himanshusainibkbiet2022@gmail.com>
2025-05-08 16:54:45 +0530

    python

--- loops-control flow/factorial.py
+++ loops-control flow/factorial.py
@@ -1,5 +1,16 @@
+# n=int(input("enter the number for factorial"))
+# fact=1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print("factorial is:",fact)
+
+
+
+
 n=int(input("enter the number for factorial"))
-fact=1
-for i in range(1,n+1):
-    fact=fact*i
-print("factorial is:",fact)
+def fact(n):
+    if n<=0:
+        return 1
+    else:
+        return n*fact(n-1)
+print(fact(n))
